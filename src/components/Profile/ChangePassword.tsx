458f2ce54e589ed7ba546fbd9e8c92456dd6d1a2
Ilya <drunktrumpet1@gmail.com>
2024-12-29 23:20:21 +0200

    fix bug change password like Eduard

--- src/components/Profile/ChangePassword.tsx
+++ src/components/Profile/ChangePassword.tsx
@@ -1,57 +1,35 @@
 import {useState} from "react";
-import {useAppDispatch, useAppSelector} from "../../app/hooks.ts";
+import {useAppDispatch} from "../../app/hooks.ts";
 import {changePassword} from "../../features/api/accountApi.ts";
-import {createToken} from "../../utils/constants.ts";
 
 interface Props {
     close: () => void;
 }
 
 const ChangePassword = ({close}: Props) => {
-    const oldPasswordLabel = 'Old password:';
-    const confirmPasswordLabel = 'Confirm password:';
-    const alertOldPassword = 'Old password is incorrect ';
-    const alertConfirmPassword = 'The new and confirmed password must be the same ';
-
     const [oldPassword, setOldPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-    const {login} = useAppSelector(state => state.user);
-
-    const [oldPasswordText, setOldPasswordText] = useState(oldPasswordLabel);
-    const [confirmPasswordText, setConfirmPasswordText] = useState(confirmPasswordLabel);
-
-    const token = useAppSelector(state => state.token);
     const dispatch = useAppDispatch();
 
     const handleClickClear = () => {
         setNewPassword('');
         setConfirmPassword('');
         setOldPassword('');
-        setOldPasswordText(oldPasswordLabel);
-        setConfirmPasswordText(confirmPasswordLabel);
     }
 
     const handleClickSave = () => {
-        const inputOldPasswordToken = createToken(login, oldPassword);
-        let textLabel;
-
-        textLabel = inputOldPasswordToken !== token ? alertOldPassword : oldPasswordLabel;
-        setOldPasswordText(textLabel);
-
-        textLabel = confirmPassword !== newPassword ? alertConfirmPassword : confirmPasswordLabel;
-        setConfirmPasswordText(textLabel);
-
-        if (inputOldPasswordToken === token && confirmPassword === newPassword) {
-            dispatch(changePassword(newPassword));
-            close();
+        if (confirmPassword === newPassword) {
+            dispatch(changePassword([newPassword, oldPassword]));
+        } else {
+            alert('new password and confirm new password are different');
         }
-
+        close();
     }
 
     return (
         <>
-            <label>{oldPasswordText}
+            <label>Old password:
                 <input
                     onChange={(e) => setOldPassword(e.target.value)}
                     value={oldPassword}
@@ -63,7 +41,7 @@ const ChangePassword = ({close}: Props) => {
                     value={newPassword}
                     type='password'/>
             </label>
-            <label>{confirmPasswordText}
+            <label>Confirm password:
                 <input
                     onChange={(e) => setConfirmPassword(e.target.value)}
                     value={confirmPassword}
